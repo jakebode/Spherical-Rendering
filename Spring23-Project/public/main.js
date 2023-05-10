852459df763fcd783925d7334cd5218e6c8fb2b9
jakebode <jbode8029@gmail.com>
2023-05-09 17:00:48 -0700

    changed branch

--- Spring23-Project/public/main.js
+++ Spring23-Project/public/main.js
@@ -6,11 +6,15 @@
 import { Icosahedron } from './modules/icosahedron.js';
 import { createShaderProgram, bindBuffers, linkAttributes } from './modules/necessaryFuncts.js';
 
-main();
+let shaderProgram;
 
-function main() {
+const main = (toDisplay) => {
     const canvas = document.querySelector('canvas');
     const gl = canvas.getContext('webgl2');
+
+    gl.deleteProgram(shaderProgram);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.DEPTH_BUFFER_BIT);
 
     if (!gl) {
         alert('WebGL2 not supported');
@@ -59,8 +63,17 @@ function main() {
     `;
 
     const buffers = bindBuffers(gl, icosahedron);
+    
+    let texData;
+    if (toDisplay===0) {
+        texData = icosahedron.getTexData();
+    } else if (toDisplay===1) {
+        texData = "images/earth.jpg";
+    } else if (toDisplay===2) {
+        texData = icosahedron.getHeightTexData();
+    }
 
-    const shaderProgram = createShaderProgram(gl, vertexSource, fragmentSource, 'images/earth.jpg');
+    shaderProgram = createShaderProgram(gl, vertexSource, fragmentSource, texData);
 
     linkAttributes(gl, buffers, shaderProgram);
 
@@ -68,33 +81,38 @@ function main() {
     gl.enable(gl.DEPTH_TEST);
 
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-
-    const uniformLocations = {
-        matrix: gl.getUniformLocation(shaderProgram, 'matrix'),
-    };
     
     const matrix = mat4.create();
     
     mat4.translate(matrix, matrix, [0, 0, 0]);
     mat4.scale(matrix, matrix, [0.5, 0.5, 0.5]);
     
+    const uniformLocation = gl.getUniformLocation(shaderProgram, 'matrix');
+    gl.uniform1i(uniformLocation, 0);
+    
     function animate() {
         requestAnimationFrame(animate);
 
-        mat4.rotateX(matrix, matrix, Math.PI/2 / 200);
+        gl.useProgram(shaderProgram);
+
+        //mat4.rotateX(matrix, matrix, Math.PI/2 / 200);
         mat4.rotateY(matrix, matrix, Math.PI/2 / 300);
         //mat4.rotateZ(matrix, matrix, Math.PI/2 / 300);
 
-        gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
+        gl.uniformMatrix4fv(uniformLocation, false, matrix);
         
         gl.disable(gl.CULL_FACE);
         gl.enable(gl.DEPTH_TEST);
 
         gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
-        gl.clearColor(0, 0, 0, 1);
+        gl.clearColor(1, 1, 1, 1);
     
         gl.drawArrays(gl.TRIANGLES, 0, icosahedron.vertexData.length / 3);
     }
-    
+
     animate();
 }
+
+main(0);
+
+export default main;
